875cd3ef3e727d108362ca457de9b8d30d47c388
Arshad vp <Arshad1071@yahoo.in>
2026-07-30 17:14:27 +0530

    Revert "save changes"
    This reverts commit eabb594af8ee1a7f8eaa3be3e566790bc5e77bab.

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ import ClubBusiness from "./pages/user/genealogy/ClubBusiness";
 import EnrollerBusiness from "./pages/user/genealogy/EnrollerBusiness";
 import StructureBusiness from "./pages/user/genealogy/StructureBusiness";
 import ListBusiness from "./pages/user/genealogy/ListBusiness";
-import EWallet from "./pages/user/financial/EWallet";
 import NotFound from "./pages/NotFound";
 import ProtectedRoute from "./routes/ProtectedRoute";
 
@@ -58,22 +57,6 @@ function App() {
           element={
             <ProtectedRoute requiredRole="user">
               <ListBusiness />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/user/financial/e-wallet"
-          element={
-            <ProtectedRoute requiredRole="user">
-              <EWallet />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/user/financial/ewallet"
-          element={
-            <ProtectedRoute requiredRole="user">
-              <EWallet />
             </ProtectedRoute>
           }
         />
