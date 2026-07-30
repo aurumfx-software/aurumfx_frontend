eabb594af8ee1a7f8eaa3be3e566790bc5e77bab
Arshad vp <Arshad1071@yahoo.in>
2026-07-30 17:12:50 +0530

    save changes

--- src/components/User/UserSidebar.jsx
+++ src/components/User/UserSidebar.jsx
@@ -35,7 +35,7 @@ const userNavItems = [
     icon: FiDollarSign,
     hasSubmenu: true,
     children: [
-      { id: "ewallet", label: "My Ewallet", path: "/user/financial/ewallet" },
+      { id: "ewallet", label: "My Ewallet", path: "/user/financial/e-wallet" },
       { id: "transfer", label: "Fund Transfer", path: "/user/financial/transfer" },
       { id: "withdrawals", label: "Withdrawals", path: "/user/financial/withdrawals" },
       { id: "investments", label: "Investments", path: "/user/financial/investments" },
